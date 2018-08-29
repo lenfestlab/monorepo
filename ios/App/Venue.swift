21a51d6c52e867bb2e366b5b7166db9259becf4b
Ajay Chainani <ajay.chainani@gmail.com>
2018-08-29 19:09:21 +0900

    Define gloss venue object

---
+++ ios/App/Venue.swift
@@ -0,0 +1,39 @@
+//
+//  Location.swift
+//  Benji
+//
+//  Created by Ajay Chainani on 8/14/18.
+//  Copyright © 2018 Lenfest. All rights reserved.
+//
+
+import UIKit
+import Gloss
+import CoreLocation
+
+struct Location: JSONDecodable {
+  let latitude: CLLocationDegrees?
+  let longitude: CLLocationDegrees?
+  
+  init?(json: JSON) {
+    self.latitude = "lat" <~~ json
+    self.longitude = "lng" <~~ json
+  }
+}
+
+struct Venue: JSONDecodable {
+  let title: String?
+  let blurb: String?
+  let link: URL?
+  let images: [URL]?
+  let location: Location?
+  
+  init?(json: JSON) {
+    self.title = "title" <~~ json
+    self.blurb = "blurb" <~~ json
+    self.link = "link" <~~ json
+    self.images = "images" <~~ json
+    self.location = "location" <~~ json
+  }
+}
+
+
