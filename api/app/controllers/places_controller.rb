4bf54a06fdaa695c9e260f0b17fd9d523fd3859c
Brent Hargrave <brent@brent.is>
2018-10-08 15:29:46 -0400

    add GET /places

---
+++ api/app/controllers/places_controller.rb
@@ -0,0 +1,23 @@
+class PlacesController < ApplicationController
+
+  def index
+    lat, lng  = query_params[:lat], query_params[:lng]
+    limit = query_params[:limit] || 20
+    coordinates = [lat, lng]
+    data = Place.preloaded_near(coordinates, limit).first(limit)
+    render json: {
+      meta: {
+        count: data.size # NOTE: Place.count throws sql error w/ geocoder
+      },
+      data: data
+    }
+  end
+
+  private
+
+  def query_params
+    params.permit(:lat, :lng, :limit)
+  end
+
+end
+
--- api/app/models/place.rb
+++ api/app/models/place.rb
@@ -10,7 +10,48 @@ class Place < ApplicationRecord
   validates :lat, :lng,
     presence: true
 
+  reverse_geocoded_by :lat, :lng
+  # NOTE: AR.includes, .count incompat w/ geocoder:
+  # https://git.io/fxZrb
+  scope :preloaded_near, -> (coordinates, limit) {
+    near(coordinates)
+      .joins(:post)
+      .preload(:post)
+  }
+
+
+  delegate :url, to: :post
+
+  [:title, :blurb, :image_url].each do |attr|
+    define_method(attr) do
+      read_attribute(attr) || post.send(attr)
+    end
+  end
+
+  def location
+    { lat: lat, lng: lng }
+  end
+
+  def as_json(options = nil)
+    super({
+      only: [
+        :identifier,
+      ],
+      methods: [
+        :title,
+        :blurb,
+        :image_url,
+        :location,
+        :post
+      ]
+    }.merge(options || {}))
+  end
+
   rails_admin do
+
+    configure :identifier do
+      hide
+    end
 
     visible false # hide from nav: https://git.io/fxmGi
 
@@ -29,9 +70,10 @@ class Place < ApplicationRecord
     object_label_method :admin_name
 
     edit do
+      include_fields :lat, :lng
       [:title, :blurb, :image_url, :radius].each do |attr|
         field attr do
-          help "Optional. Only use override the parent Post value above in notifications."
+          help "Optional. Only use to override the parent Post value above in notifications."
         end
       end
     end
