b0f3891db141e66cb6bb0ca5aebb627321a1c0ba
Brent Hargrave <brent@brent.is>
2018-10-22 15:17:12 -0400

    routes for /about, /privacy, /tos

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -5,5 +5,10 @@ Rails.application.routes.draw do
   # NOTE: deprecated
   resources :posts, only: :index
 
+  # static pages
+  get "/privacy", to: redirect("privacy.html")
+  get "/tos", to: redirect("tos.html")
+  get "/about", to: redirect("https://medium.com/the-lenfest-local-lab")
+
   get "force_exception" => "application#force_exception"
 end
