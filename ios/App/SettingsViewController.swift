66cd604b327a7c6078bae8a41816bddea6eb4b87
Brent Hargrave <brent@brent.is>
2018-10-23 20:23:15 -0400

    update Settings view copy (#62)
    per revised copy spec: https://goo.gl/uY4WhC

--- ios/App/SettingsViewController.swift
+++ ios/App/SettingsViewController.swift
@@ -4,7 +4,7 @@ import UserNotifications
 import SafariServices
 
 class SettingsViewController: UITableViewController, SettingsToggleCellDelegate, LocationManagerAuthorizationDelegate {
-  
+
   let locationManager = LocationManager.shared
   private var notification: NSObjectProtocol?
 
@@ -22,22 +22,22 @@ class SettingsViewController: UITableViewController, SettingsToggleCellDelegate,
 
   deinit {
     // make sure to remove the observer when this view controller is dismissed/deallocated
-    
+
     if let notification = notification {
       NotificationCenter.default.removeObserver(notification)
     }
   }
-  
+
   func authorized(_ locationManager: LocationManager, status: CLAuthorizationStatus) {
     self.loadSettings()
     self.tableView.reloadData()
   }
-  
+
   func notAuthorized(_ locationManager: LocationManager, status: CLAuthorizationStatus) {
     self.loadSettings()
     self.tableView.reloadData()
   }
-  
+
   func switchTriggered(sender: UISwitch) {
     switch sender.tag {
     case 0:
@@ -54,7 +54,7 @@ class SettingsViewController: UITableViewController, SettingsToggleCellDelegate,
         // If general location settings are enabled then open location settings for the app
         UIApplication.shared.open(url, options: [:], completionHandler: nil)
       }
-      
+
     case 1:
       print("Access Location")
       analytics.log(.changeLocationSettings(enabled: sender.isOn))
@@ -74,13 +74,13 @@ class SettingsViewController: UITableViewController, SettingsToggleCellDelegate,
       print("unknown switch")
     }
   }
-  
+
   func loadSettings() {
-    
+
     var rows = [
       [
         "identifier": "default",
-        "title":"About Us",
+        "title":"About us",
         "path":"about",
         ],
       [
@@ -90,64 +90,64 @@ class SettingsViewController: UITableViewController, SettingsToggleCellDelegate,
         ],
       [
         "identifier": "default",
-        "title":"Term of Service",
+        "title":"Terms of Service",
         "path":"tos",
         "inset":"zero",
         ]
     ]
-    
+
     if MFMailComposeViewController.canSendMail() {
       rows.append([
         "identifier": "button",
-        "title":"Leave Your Feedback",
-        "action":"feedback"
+        "title": "Share your feedback",
+        "action": "feedback"
         ])
     }
-    
+
     self.settings =
       [
         [
-          "title": "PERMISSIONS",
+          "title": "Permissions",
           "rows": [
             [
               "identifier": "setting",
-              "title":"Enable Notifications",
-              "description":"This App uses push notifications to send you related articles based on where you are.",
+              "title": "Enable notifications",
+              "description": "This app uses notifications to send related articles based on where you are.",
               "toggle": notificationManager.authorizationStatus == .authorized
             ],
             [
               "identifier": "setting",
-              "title":"Access Location",
-              "description":"This App serves best with access to your location. Map and notification features uses your location to display and send content from your nearby locations.",
+              "title": "Enable location",
+              "description": "Map and notification features use your location to display and send you articles.",
               "toggle": CLLocationManager.authorizationStatus() == .authorizedAlways
             ],
             [
               "identifier": "setting",
-              "title":"Clear History",
-              "description":"We remember which notifications you have already seen. Turn this off to clear your history, so you can get the notifications again.",
+              "title": "Recurring notifications",
+              "description": "We remember which notifications you receive and don’t send them again. Turn this off to receive each notification again.",
               "toggle": true
             ]
 
           ]
         ],
         [
-          "title": "GENERAL",
+          "title": "General",
           "rows": rows
         ]
-        
+
     ]
-    
+
   }
-  
+
   var settings:[[String:Any?]] = [[:]]
-  
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    
+
     self.tableView.separatorColor = UIColor.init(red: 241/255, green: 221/255, blue: 187/255, alpha: 1)
-    
+
     locationManager.authorizationDelegate = self
-    
+
     loadSettings()
     notification = NotificationCenter.default.addObserver(forName: .UIApplicationWillEnterForeground, object: nil, queue: .main) {
       [unowned self] notification in
@@ -158,47 +158,47 @@ class SettingsViewController: UITableViewController, SettingsToggleCellDelegate,
         }
       })
     }
-    
+
     self.title = "Settings"
-    
+
     self.tableView.backgroundColor = UIColor.white
-    
+
     tableView.rowHeight = UITableViewAutomaticDimension
     tableView.estimatedRowHeight = 125
-    
+
     let nib = UINib.init(nibName: "SettingsToggleCell", bundle: nil)
     tableView.register(nib, forCellReuseIdentifier: "setting")
-    
+
     let buttonNib = UINib.init(nibName: "ButtonCell", bundle: nil)
     tableView.register(buttonNib, forCellReuseIdentifier: "button")
-    
+
     tableView.register(UITableViewCell.self, forCellReuseIdentifier: "default")
-    
+
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem
   }
-  
+
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-  
+
   // MARK: - Table view data source
-  
+
   override func numberOfSections(in tableView: UITableView) -> Int {
     return settings.count
   }
-  
+
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     let section = settings[section]
     let rows = section["rows"] as! [Any]
     return rows.count
   }
-  
+
   override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
     return CGFloat(55);
   }
-  
+
   override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
     let section = settings[section]
     let title = section["title"] as! String
@@ -208,13 +208,13 @@ class SettingsViewController: UITableViewController, SettingsToggleCellDelegate,
     label.font =  UIFont(name: "WorkSans-Medium", size: 16)
     return label
   }
-  
+
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let section = settings[indexPath.section]
     let rows = section["rows"] as! [Any]
     let row = rows[indexPath.row] as! [String:Any]
     let identifier = row["identifier"] as! String
-    
+
     if identifier == "setting" {
       let cell:SettingsToggleCell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath) as! SettingsToggleCell
       cell.titleLabel.text = row["title"] as? String
@@ -242,7 +242,7 @@ class SettingsViewController: UITableViewController, SettingsToggleCellDelegate,
       return cell
     }
   }
-  
+
   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     let section = settings[indexPath.section]
     let rows = section["rows"] as! [Any]
@@ -258,13 +258,13 @@ class SettingsViewController: UITableViewController, SettingsToggleCellDelegate,
       }
     }
   }
-  
+
 }
 
 import MessageUI
 
 extension SettingsViewController: MFMailComposeViewControllerDelegate {
-  
+
   func sendFeedback(to: [String], subject: String) {
     let mailComposerVC = MFMailComposeViewController()
     mailComposerVC.mailComposeDelegate = self
@@ -273,9 +273,9 @@ extension SettingsViewController: MFMailComposeViewControllerDelegate {
     mailComposerVC.setMessageBody("", isHTML: false)
     present(mailComposerVC, animated: true, completion: nil)
   }
-  
+
   func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
     controller.dismiss(animated: true, completion: nil)
   }
-  
+
 }
