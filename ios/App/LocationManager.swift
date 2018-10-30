5cec43031d29cb7a896ae5ed35b2fff69d94821d
Ajay Chainani <ajay.chainani@gmail.com>
2018-10-30 11:47:53 +0900

    Trim whitespace from LocationManager.swift (#70)

--- ios/App/LocationManager.swift
+++ ios/App/LocationManager.swift
@@ -37,44 +37,44 @@ class LocationManager: NSObject, CLLocationManagerDelegate {
     super.init()
     locationManager.delegate = self
   }
-  
+
   func enableBasicLocationServices() {
     let status = CLLocationManager.authorizationStatus()
     authorizationStatusUpdated(status: status)
   }
-  
+
   func authorizationStatusUpdated(status: CLAuthorizationStatus) {
     switch status {
     case .notDetermined:
       // Request when-in-use authorization initially
       locationManager.requestAlwaysAuthorization()
       break
-      
+
     case .restricted, .denied:
       authorized = false
       authorizationDelegate?.notAuthorized(self, status: status)
       break
-      
+
     case .authorizedWhenInUse, .authorizedAlways:
       authorized = true
       authorizationDelegate?.authorized(self, status: status)
       break
     }
   }
-  
+
   func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
     if status != .notDetermined {
       authorizationStatusUpdated(status: status)
     }
   }
-  
+
   func locationManager(_ manager: CLLocationManager,
                        didUpdateLocations locations: [CLLocation]) {
     let latestLocation: CLLocation = locations[locations.count - 1]
     let latitude = String(latestLocation.coordinate.latitude)
     let longitude = String(latestLocation.coordinate.longitude)
     print("\(latitude) \(longitude)")
-    
+
     self.delegate?.locationUpdated(self, coordinate: latestLocation.coordinate)
   }
 
@@ -82,15 +82,15 @@ class LocationManager: NSObject, CLLocationManagerDelegate {
     print("Monitoring failed for region with identifier: \(region!.identifier)")
     print(error)
   }
-  
+
   func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
     print("Location Manager failed with the following error: \(error)")
   }
 
   func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
     if region is CLCircularRegion {
-      self.delegate?.regionEngtered(self, region: region as! CLCircularRegion)      
+      self.delegate?.regionEngtered(self, region: region as! CLCircularRegion)
     }
   }
-  
+
 }
