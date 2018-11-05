7bc3837dc8c5baed522e6da6f1e9be90809e0790
Brent Hargrave <brent@brent.is>
2018-11-05 10:15:41 -0500

    onboarding: update copy, add arrows to overlay

--- ios/App/PermissionsViewController.swift
+++ ios/App/PermissionsViewController.swift
@@ -60,8 +60,12 @@ class PermissionsViewController: UIViewController, LocationManagerAuthorizationD
 
   @IBAction func done(sender: UIButton) {
     self.analytics.log(.tapsEnableLocationButton)
+    let env = Env()
 
-    let alertController = UIAlertController(title: "Allow \"HERE\" to access your location?", message: "We monitor your location to notify you when you are near interesting spots.", preferredStyle: .alert)
+    let alertController = UIAlertController(
+      title: "Allow \"\(env.appName)\" to access your location?",
+      message: "\(env.appName) uses your location to send you news about places you go. Please Always Allow access.",
+      preferredStyle: .alert)
 
     let action1 = UIAlertAction(title: "Only While Using the App", style: .default) { (action:UIAlertAction) in
       print("Only While Using the App");
@@ -83,15 +87,17 @@ class PermissionsViewController: UIViewController, LocationManagerAuthorizationD
 
     self.present(alertController, animated: true, completion: nil)
 
-    let gray = UIView(frame: CGRect.init(x: 0, y: 0, width: 270, height: 252))
+    let grayH: CGFloat = 267.5 // from view hierarchy debugger
+    let gray = UIView(frame: CGRect.init(x: 0, y: 0, width: 270, height: grayH))
     gray.backgroundColor = UIColor.black.withAlphaComponent(0.1)
     gray.layer.cornerRadius = 11.0
-    gray.clipsToBounds = true
     gray.isUserInteractionEnabled = false
     alertController.view.addSubview(gray)
-    alertController.view.clipsToBounds = true
 
-    let maskRect = CGRect.init(x: 0, y: 163, width: 270, height: 44)
+    let maskY: CGFloat = 179 // eyeballed
+    let maskH: CGFloat = 44
+    let maskW: CGFloat = 270
+    let maskRect = CGRect(x: 0, y: maskY, width: maskW, height: maskH)
     let invert = true
     let viewToMask = gray
 
@@ -110,12 +116,33 @@ class PermissionsViewController: UIViewController, LocationManagerAuthorizationD
     // Set the mask of the view.
     viewToMask.layer.mask = maskLayer;
 
+    // arrows
+    let arrowsContainerView = UIView(frame: gray.frame)
+    arrowsContainerView.isUserInteractionEnabled = false
+    arrowsContainerView.backgroundColor = .clear
+    alertController.view.addSubview(arrowsContainerView)
+    ["Left", "Right"].forEach { imageNameSuffix in
+      let arrowImageName = "arrow\(imageNameSuffix)"
+      let arrowImage = UIImage(named: arrowImageName)!
+      let arrowImageView = UIImageView(image: arrowImage)
+      arrowImageView.isUserInteractionEnabled = false
+
+      arrowsContainerView.addSubview(arrowImageView)
+
+      let size = arrowImage.size
+      let width = size.width
+      let widthHalf = width / 2
+      let center: CGPoint
+      let centerY: CGFloat = (maskY + (maskH / 2))
+      let imageSuperviewInset: CGFloat = 40 // eyeballed
+      center = (imageNameSuffix == "Left")
+        ? CGPoint(x: widthHalf - imageSuperviewInset, y: centerY)
+        : CGPoint(x: (maskW - widthHalf) + imageSuperviewInset, y: centerY)
+      arrowImageView.center = center
+
+    }
+
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-  
 }
 
