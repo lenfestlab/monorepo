b9a13841f6f87c7470485e02bf7638060d0ee727
Brent Hargrave <brent@brent.is>
2018-11-06 13:29:16 -0500

    seed script

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+raise "attempted to seed prod!" if Rails.env == "production"
+
+dir = ENV["ADMIN_DB_SEED_DIR"]
+file = File.open("#{dir}/posts.yml")
+data = YAML::load(file)
+
+data.each do |post_data|
+  places_data = post_data.delete("places")
+  post = Post.create(post_data)
+  places_data.each do |place_data|
+    post.places.create(place_data)
+  end
+end
