019fe96572deadaf603d0e03b7e3b92fe1eb9d52
Brent Hargrave <brent@brent.is>
2018-12-12 12:18:31 -0800

    compress API responses

--- api/app/controllers/places_controller.rb
+++ api/app/controllers/places_controller.rb
@@ -1,6 +1,9 @@
 class PlacesController < ApplicationController
 
+  before_action :force_compression
+
   def index
+
     lat, lng  = query_params[:lat], query_params[:lng]
     limit = query_params[:limit].to_i || 20
     coordinates = [lat, lng]
@@ -18,6 +21,12 @@ class PlacesController < ApplicationController
 
   def query_params
     params.permit(:lat, :lng, :limit)
+  end
+
+  private
+
+  def force_compression
+    request.env['HTTP_ACCEPT_ENCODING'] = 'gzip'
   end
 
 end
