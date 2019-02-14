c1b0ba7bc24efe176f7288ec205baf2fd5fad659
Ajay Chainani <ajay.chainani@gmail.com>
2019-02-14 10:46:31 -0500

    [ios] Remove default google.com url

--- food/ios/App/CategoryDataStore.swift
+++ food/ios/App/CategoryDataStore.swift
@@ -11,16 +11,15 @@ class CategoryDataStore: NSObject {
     let url = "\(env.apiBaseUrlString)/categories.json"
 
     Alamofire.request(url, method: .get, parameters: nil, encoding: URLEncoding.default, headers: nil).responseJSON { response in
-      let json = response.result.value as? JSON
-      if (json == nil) {
+      guard let json = response.result.value as? JSON else {
         DispatchQueue.main.async { completion(false, [], 0) }
         return
       }
       var count = 0
-      if let meta = json!["meta"] as? JSON {
+      if let meta = json["meta"] as? JSON {
         count = meta["count"] as? Int ?? 0
       }
-      guard let placesJSON = json!["data"] as? [JSON] else {
+      guard let placesJSON = json["data"] as? [JSON] else {
         DispatchQueue.main.async { completion(false, [], 0) }
         return
       }
