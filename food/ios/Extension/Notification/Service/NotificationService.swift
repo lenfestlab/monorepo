0f7a76a609658bacf98610896bed851547722fc4
Brent Hargrave <brent@brent.is>
2019-04-28 18:31:58 -0400

    [food][ios] extract duplicate code into framework (#198)

--- food/ios/Extension/Notification/Service/NotificationService.swift
+++ food/ios/Extension/Notification/Service/NotificationService.swift
@@ -1,4 +1,5 @@
 import UserNotifications
+import Shared
 
 class NotificationService: UNNotificationServiceExtension {
 
@@ -30,28 +31,6 @@ class NotificationService: UNNotificationServiceExtension {
       let bestAttemptContent =  bestAttemptContent {
       contentHandler(bestAttemptContent)
     }
-  }
-
-}
-
-// TODO: share w/ both main app and extension using framework
-extension UNNotificationAttachment {
-
-  static func create(_ imageFileName: String, data: NSData, options: [NSObject : AnyObject]?) -> UNNotificationAttachment? {
-    let fileManager = FileManager.default
-    let tmpSubFolderName = ProcessInfo.processInfo.globallyUniqueString
-    let tmpSubFolderURL = URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(tmpSubFolderName, isDirectory: true)
-    do {
-      try fileManager.createDirectory(at: tmpSubFolderURL, withIntermediateDirectories: true, attributes: nil)
-      let imageFileIdentifier = imageFileName+".jpg"
-      let fileURL = tmpSubFolderURL.appendingPathComponent(imageFileIdentifier)
-      try data.write(to: fileURL, options: [])
-      let imageAttachment = try UNNotificationAttachment(identifier: imageFileIdentifier, url: fileURL, options: options)
-      return imageAttachment
-    } catch let error {
-      print("error \(error)")
-    }
-    return nil
   }
 
 }
