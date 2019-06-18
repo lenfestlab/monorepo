d20d8abbf99c2086144f7cb2e5c060f169af09fa
Brent Hargrave <brent@brent.is>
2019-06-18 09:44:30 -0400

    setup new food app sans icloud
    - more icloud cleanup
    - copy firebase config file by bundle id
    - unversion app name config

--- food/ios/scripts/copy_firebase_config.sh
+++ food/ios/scripts/copy_firebase_config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-PLIST_PATH="${SRCROOT}/firebase/project/${ENV_NAME}/GoogleService-Info.plist"
+PLIST_PATH="${SRCROOT}/firebase/project/${PRODUCT_BUNDLE_IDENTIFIER}/GoogleService-Info.plist"
 echo ${PLIST_PATH}
 cp -f ${PLIST_PATH} "${SRCROOT}/App/Resources"
