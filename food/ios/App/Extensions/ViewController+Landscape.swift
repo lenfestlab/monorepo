6b834a5c689460673815fd034723be043632f406
Brent Hargrave <brent@brent.is>
2019-08-05 15:39:11 -0400

    [food][ios] add landscape view to photo carousel

---
+++ food/ios/App/Extensions/ViewController+Landscape.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+extension TabBarViewController {
+  override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+    return [.portrait]
+  }
+}
+
+extension MainController {
+  override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+    return [.portrait]
+  }
+}
+
+extension DetailViewController {
+  override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+    return [.portrait]
+  }
+}
+
