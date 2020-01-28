7fc607579bbd29a0c3edfa70e3effaecadf4f211
Ajay Chainani <ajay.chainani@gmail.com>
2020-01-28 13:54:51 -0500

    [newsletter][editor] Initial editor interface commit

---
+++ nabeletter/editor/header.js
@@ -0,0 +1,3 @@
+function header() {
+    return `Logo/ Title/Date/ Lead Image`
+}
---
+++ nabeletter/editor/history.js
@@ -0,0 +1,14 @@
+function history(result) {
+    const title = result["title"]
+    const images = result["images"]
+    const caption = result["caption"]
+
+    return `
+<table class="section">
+<tr>
+${images.map(element => `<td><table><tr><td><img src="${element.image}"></td></tr></table></td>`).join('')}
+</tr>
+<tr><td class="caption" colspan="2">${caption}</td></tr>
+</table>
+`
+}
