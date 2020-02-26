b284225885815a288f4f6cd19fe5939bc649503a
Brent Hargrave <brent@brent.is>
2020-02-26 11:53:25 -0500

    setup cors; reenable delivery

---
+++ nabeletter/web/config/initializers/cors.rb
@@ -0,0 +1,9 @@
+# http://bit.ly/2PoJrhK
+Rails.application.config.middleware.insert_before 0, Rack::Cors, debug: true, logger: (-> { Rails.logger }) do
+  allow do
+    origins '*'
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
