eae234fb3094253fbbf8524dcfeeb7dbc94534bd
Brent Hargrave <brent@brent.is>
2020-04-02 11:45:54 -0700

    [nabeletter] editor request auth

--- nabeletter/web/app/controllers/resource_controller.rb
+++ nabeletter/web/app/controllers/resource_controller.rb
@@ -1,5 +1,10 @@
 class ResourceController < ApplicationController
   include JSONAPI::ActsAsResourceController
 
-  before_action :authenticate_user!
+  before_action do |controller|
+    origin = request.headers['origin']
+    unless origin && origin.include?(ENV["EDITOR_HOST"])
+      controller.send :authenticate_user!
+    end
+  end
 end
