333d39c7c937e39b2f6d31ae77e59425ac7bc186
Brent Hargrave <brent@brent.is>
2021-03-05 11:52:16 -0800

    [nabeletter] news: editable site names

--- nabeletter/web/app/javascript/components/admin/editions/shared/EditionBodyInput/sections/news/index.ts
+++ nabeletter/web/app/javascript/components/admin/editions/shared/EditionBodyInput/sections/news/index.ts
@@ -7,10 +7,14 @@ export interface Article {
   image: string
 }
 
+export interface EditableArticle extends Article {
+  site_name_custom?: string | null
+}
+
 import { SectionConfig } from "../section"
 export interface Config extends SectionConfig {
   url: string
-  articles: Article[]
+  articles: EditableArticle[]
 }
 export type SetConfig = (config: Config) => void
 
